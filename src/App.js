3c727be8c5a9436c06641863afc16da49758f225
Hyunjun Lee <leehyunjune11@gmail.com>
2024-08-28 18:06:14 +0900

    [240828] 1~3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import AppLayout from './layout/AppLayout';
+import Homepage from './pages/Hompage/Homepage';
+import MoviePage from './pages/Movies/MoviePage';
+import MovieDetailPage from './pages/MovieDetail/MovieDetailPage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+
+// 홈페이지 /
+// 영화 전체보여주는 페이지(서치) /movies
+// 영화 디테일 페이지 /movies/:id
+// <Route index element={<Homepage/>} /> index는 path="/"를 의미
 
 function App() {
   return (
     <div>
+      <Routes>
+        <Route path="/" element={<AppLayout/>}>
+          <Route index element={<Homepage/>} /> 
+          <Route path="movies">
+            <Route index element={<MoviePage/>}/>
+            <Route path=":id" element={<MovieDetailPage/>} />
+          </Route>
+          {/* { <Route path="/movies" element={<MoviePage/>} />
+          <Route path="/movies/:id" element={<MovieDetailPage/>} /> 
+          위랑 같은 의미 Movies를 쓰는 라우트들을 묶어줌
+          } */}
+        </Route>
 
+        <Route path="*" element={<NotFoundPage/>}/>     
+      </Routes>
     </div>
   );
 }
